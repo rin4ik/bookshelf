dec8b35a1b3e7dd24b44cd770df25817214a27d0
Mirakhmad <m.mirakhmad@gmail.com>
2022-08-05 15:34:21 +0500

    styled components

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -16,30 +16,28 @@ import {Dialog as ReachDialog} from '@reach/dialog'
 // 💰 in my finished version I have: Button, Input, CircleButton, Dialog, FormGroup
 
 // 🎨 here are a bunch of styles you can copy/paste if you want
-// Button:
-//   padding: '10px 15px',
-//   border: '0',
-//   lineHeight: '1',
-//   borderRadius: '3px',
-
-// Button variant="primary" (in addition to the above styles)
-//   background: '#3f51b5',
-//   color: 'white',
-
-// Button variant="secondary" (in addition to the above styles)
-//   background: '#f1f2f7',
-//   color: '#434449',
-
-// Input
-//   borderRadius: '3px',
-//   border: '1px solid #f1f1f4',
-//   background: '#f1f2f7',
-//   padding: '8px 12px',
-
-// FormGroup
-//   display: 'flex',
-//   flexDirection: 'column',
-
+// Button: 
+const Button = styled.button(props => {
+  return {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+    color: props.variant === 'primary' ? 'white' : '#434449',
+    background: props.variant === 'primary' ? '#3f51b5' : '#f1f2f7',
+  }
+})
+ 
+const Input = styled.input({
+  borderRadius: '3px',
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px',
+})
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
 // 💰 I'm giving a few of these to you:
 const CircleButton = styled.button({
   borderRadius: '30px',
@@ -68,4 +66,4 @@ const Dialog = styled(ReachDialog)({
   },
 })
 
-export {CircleButton, Dialog}
+export {CircleButton, Dialog, FormGroup, Input, Button}
